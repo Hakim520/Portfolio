f88b19f3036ee48ead0ed6dce7587a3493d82095
Hakim <95052573+Hakim520@users.noreply.github.com>
2024-12-05 15:12:35 +0100

    some changes in the front

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -40,7 +40,8 @@ export const Contact = () => {
             method:"POST",
             headers:
                 {
-                    "Content-Type": "application/json;charset=utf-8",                    },
+                    "Content-Type": "application/json;charset=utf-8",
+                },
             body:
                 JSON.stringify(formDetails),
                 });
