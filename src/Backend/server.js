f9cccbed6795cd26914e1654fc7e44764b68106c
Hakim <95052573+Hakim520@users.noreply.github.com>
2024-12-05 15:02:31 +0100

    :heavy_plus_sign: adding a port on the SS

--- src/Backend/server.js
+++ src/Backend/server.js
@@ -18,8 +18,10 @@ app.use(cors(
 ));
 app.use(express.json());
 app.use("/", router);
-// app.listen(3000, () => 
-console.log("Server Running");
+const PORT = process.env.PORT || 3000; 
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
 
 
 const contactEmail = nodemailer.createTransport({
